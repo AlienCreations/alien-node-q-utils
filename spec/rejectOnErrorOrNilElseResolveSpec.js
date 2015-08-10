f1bb55125a8cd11d5aabebc592b91276c3f593f5
Sean Cannon <alienwebguy@gmail.com>
2015-08-09 17:40:22 -0700

    0.0.5 - add rejectOnErrorOrNilElseResolve() method and spec

--- spec/rejectOnErrorOrNilElseResolveSpec.js
+++ spec/rejectOnErrorOrNilElseResolveSpec.js
@@ -17,24 +17,39 @@ var mockPromiseWithError = function(err) {
   return deferred.promise;
 };
 
-var FAKE_RESOLVE_WITH = 'Success!',
-    FAKE_ERR          = { code : 6000, message : 'Failed!' };
+var FAKE_RESOLVE_WITH           = 'Success!',
+    FAKE_RESOLVE_WITH_NULL      = null,
+    FAKE_RESOLVE_WITH_UNDEFINED = undefined,
+    FAKE_ERR                    = {code : 6000, message : 'Failed!'};
 
 describe('rejectOnErrorOrResolve', function() {
 
-  it('should resolve a promise where no error is provided', function(done) {
+  it('should resolve a promise where no error is provided and resolveWith is not Nil', function(done) {
     mockPromiseNoError(FAKE_RESOLVE_WITH).then(function(result) {
       expect(result).toBe(FAKE_RESOLVE_WITH);
       done();
     });
   });
 
-  it('should reject a promise where an error is provided', function(done) {
-    mockPromiseWithError(FAKE_ERR).catch(function(err) {
-      expect(err).toBe(FAKE_ERR);
+  it('should reject a promise when resolveWith is null [Nil]', function() {
+    mockPromiseNoError(FAKE_RESOLVE_WITH_NULL).catch(function(err) {
+      expect(err).toBe(undefined);
       done();
     })
   });
 
-});
+  it('should reject a promise when resolveWith is undefined [Nil]', function() {
+    mockPromiseNoError(FAKE_RESOLVE_WITH_UNDEFINED).catch(function(err) {
+      expect(err).toBe(undefined);
+      done();
+    })
+  });
 
+  it('should reject a promise where an error is provided', function(done) {
+    mockPromiseWithError(FAKE_ERR).catch(function(err) {
+      expect(err).toBe(FAKE_ERR);
+      done();
+    });
+  });
+
+});
